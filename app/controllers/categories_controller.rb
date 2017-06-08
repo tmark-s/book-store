22a3b6e8122ea6edaf05abd07053600ed0b2be78
book-store by tmark-s <tmark_s@hotmail.com>
2017-06-08 17:39:32 +0700

    finish about user category and book

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,49 +1,49 @@
 class CategoriesController < ApplicationController
-	def index
-		@categories = Category.all
-	end
+  def index
+    @categories = Category.all
+  end
 
-	def new
-		@category = Category.new
-	end
+  def new
+    @category = Category.new
+  end
 
-	def create
-		@category = Category.new(article_params)
-		if @category.save
-			flash[:success] = "Category was created"
-			redirect_to categories_path
-		else
-			render 'new'
-		end
-	end	
+  def create
+    @category = Category.new(article_params)
+    if @category.save
+      flash[:success] = "Category was created"
+      redirect_to categories_path
+    else
+      render 'new'
+    end
+  end
 
-	def edit
-		@category = Category.find(params[:id])
-	end
+  def edit
+    @category = Category.find(params[:id])
+  end
 
-	def update
-		@category = Category.find(params[:id])
-		if @category.update(article_params)
-			flash[:success] = "Category was updated"
-			redirect_to categories_path
-		else
-			render 'edit'
-		end
-	end
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(article_params)
+      flash[:success] = "Category was updated"
+      redirect_to categories_path
+    else
+      render 'edit'
+    end
+  end
 
-	def show
-		@category = Category.find(params[:id])
-	end
+  def show
+    @category = Category.find(params[:id])
+  end
 
-	def destroy
-		@category = Category.find(params[:id])
-		@category.destroy
-		flash[:danger] = "Category was deleted"
-		redirect_to categories_path
-	end
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    flash[:danger] = "Category was deleted"
+    redirect_to categories_path
+  end
 
-	private
-	def article_params
-		params.require(:category).permit(:categoryname)
-	end
-end
+  private
+  def article_params
+    params.require(:category).permit(:categoryname)
+  end
+end 

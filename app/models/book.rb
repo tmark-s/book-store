ec2c1bc31e20b3c1f9a89bdcd94fa395a6c171bd
book-store by tmark-s <tmark_s@hotmail.com>
2017-06-09 11:27:01 +0700

    Edit about category show page

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,11 @@
 class Book < ActiveRecord::Base
-	belongs_to :category
+  belongs_to :category
 
-	mount_uploader :image, ImageUploader
+  mount_uploader :image, ImageUploader
+
+  validates :title, presence: true
+
+  validates :author, presence: true
+
+  validates :price, presence: true
 end
